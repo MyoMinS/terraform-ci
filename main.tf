bfd1b771d17fe15f416585b19c230567692c7c90
MyoMinS <myominsoe432@gmail.com>
2024-11-10 22:19:20 +0630

    add main.tf

--- main.tf
+++ main.tf
@@ -5,10 +5,9 @@ terraform {
       name = "terraform-ci-github-action"
     }
   }
-  
 }
 resource "null_resource" "example" {
   triggers = {
-    value = "A example resource that does nothing!"
+    value = "A example resource that does nothing!!"
   }
 }
