44f65393e0a8b690faf8e00a4f9c9357bb012596
MyoMinS <myominsoe432@gmail.com>
2024-11-10 22:21:01 +0630

    add main.tf

--- main.tf
+++ main.tf
@@ -8,6 +8,6 @@ terraform {
 }
 resource "null_resource" "example" {
   triggers = {
-    value = "A example resource that does nothing!!"
+    value = "A example resource that does nothing!!!"
   }
 }
