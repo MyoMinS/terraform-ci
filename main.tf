72a362f9563c51eee047440f5f142dc3046e047b
MyoMinS <myominsoe432@gmail.com>
2024-11-10 22:03:17 +0630

    add main.tf

---
+++ main.tf
@@ -0,0 +1,14 @@
+terraform {
+  backend "remote" {
+    organization = "myomins"
+    workspaces {
+      name = "terraform-ci-github-action"
+    }
+  }
+  
+}
+resource "null_resource" "example" {
+  triggers = {
+    value = "A example resource that does nothing!"
+  }
+}
